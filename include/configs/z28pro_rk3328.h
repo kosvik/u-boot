35ca886d280204b2850bf3895bf5c2f4f6fa3495
Konstantin Evseenko <kosvik@gmail.com>
2018-05-14 21:42:55 +0200

    added z28 pro board

---
+++ include/configs/z28pro_rk3328.h
@@ -0,0 +1,18 @@
+/*
+ * (C) Copyright 2018 Slon
+ *
+ * SPDX-License-Identifier:     GPL-2.0+
+ */
+
+#ifndef __Z28PRO_RK3328_H
+#define __Z28PRO_RK3328_H
+
+#include <configs/rk3328_common.h>
+
+#define CONFIG_SYS_MMC_ENV_DEV 1
+
+#define SDRAM_BANK_SIZE			(2UL << 30)
+
+#define CONFIG_CONSOLE_SCROLL_LINES		10
+
+#endif
